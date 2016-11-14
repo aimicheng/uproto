1c6c420f0883a690aa096e8c2c2e67ad728b07e3
chengliu <chengliu@sohu-inc.com>
2016-11-14 18:58:40 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,124 @@
+var types_defined = {};
+var namespace_cache = {};
+var EMPTY_FUNC = function() {};
+var NS_SP = '.';
+
+var uproto = {
+    ns: 'default',
+    import: function(TypeName) {
+        var namespace_str = this.ns,
+            pos = TypeName.lastIndexOf(NS_SP);
+        if (pos > -1) {
+            namespace_str = TypeName.substr(0, pos);
+            TypeName = TypeName.substr(pos + 1);
+        }
+        var ns = this._resolve_namespace(namespace_str);
+
+        if (!ns[TypeName]) {
+            throw new Error('Type ' + TypeName + ' not found in namespace ' + namespace_str);
+        }
+
+        return ns[TypeName];
+    },
+    _resolve_namespace: function(namespace_str) {
+        if (!namespace_str) {
+            return types_defined['default'];
+        }
+
+        if (namespace_cache[namespace_str]) {
+            return namespace_cache[namespace_str];
+        }
+
+        var parts = namespace_str.split(NS_SP),
+            node = types_defined;
+        while (parts.length > 0) {
+            var name = parts.shift();
+            if (typeof node[name] !== 'object') {
+                throw new Error('namespace not found.');
+            }
+            node = node[name];
+        }
+
+        namespace_cache[namespace_str] = node;
+        return node;
+    },
+    //get or set current namespace;
+    namespace: function(namespace_str) {
+        if (!namespace_str) {
+            return this.ns;
+        }
+        var parts = namespace_str.split(NS_SP),
+            node = types_defined,
+            ns_path = [];
+        while (parts.length > 0) {
+            var name = parts.shift();
+            ns_path.push(name);
+            if (!node[name]) {
+                node[name] = {
+                    __ns_name: ns_path.join(NS_SP)
+                };
+            }
+            node = node[name];
+        }
+        this.ns = namespace_str;
+    },
+    merge: function(dest_obj, src_obj) {
+        Object.getOwnPropertyNames(src_obj).forEach(function(name) {
+            dest_obj[name] = src_obj[name];
+        });
+        return dest_obj;
+    },
+    extends: function(TypeName, ParentType, prototype_obj) {
+        //check params
+        if (typeof ParentType === 'string') {
+            ParentType = this.import(ParentType);
+        }
+
+        if (typeof ParentType !== 'function') {
+            throw new Error('param ParentType must be a function.');
+        }
+        if (typeof TypeName !== 'string') {
+            throw new Error('param TypeName must be a string.');
+        }
+        if (typeof prototype_obj !== 'object') {
+            throw new Error('param prototype_obj must be an object.');
+        }
+
+        //resolve namespace_str
+        var namespace_str = this.ns,
+            pos = TypeName.lastIndexOf(NS_SP);
+        if (pos > -1) {
+            namespace_str = TypeName.substr(0, NS_SP);
+        }
+        var ns = this._resolve_namespace(namespace_str);
+        //forbid redefine
+        if (ns[TypeName]) {
+            throw new Error('type already defined: ' + TypeName);
+        }
+
+        if (!prototype_obj) {
+            prototype_obj = {};
+        }
+        if (!prototype_obj.init) {
+            prototype_obj.init = EMPTY_FUNC;
+        }
+
+        var T = ns[TypeName] = function () {
+            ParentType.apply(this, arguments);
+
+            //call parent init
+            if (ParentType.prototype.init) {
+                ParentType.prototype.init.apply(this, arguments);
+            }
+            this.init(arguments);
+        }
+
+        T.prototype = Object.create(ParentType.prototype);
+        this.merge(T.prototype, prototype_obj);
+        T.prototype.constructor = T;
+        return T;
+    }
+};
+
+uproto.namespace('default');
+module.exports = uproto;
---
+++ tests/test-class.js
@@ -0,0 +1,63 @@
+var uproto = require('../index');
+var assert = require('assert');
+
+
+it('namespace should pass', function() {
+    uproto.namespace('test');
+    var current_ns = uproto.namespace();
+    assert(current_ns === 'test');
+});
+
+it('extends should pass', function() {
+    var A = uproto.extends('A', Object, {
+        init: function(opt) {
+            this.a = 'a';
+        },
+        test_a: function() {
+            return "func a";
+        },
+        overwrite: function() {
+            return "in func a";
+        }
+    });
+    var a = new A({
+        name: 'a'
+    });
+    assert(a.init);
+    assert(a.a === 'a');
+    assert(a.test_a);
+    assert(a.test_a() === 'func a');
+    assert(a.overwrite);
+    assert(a.overwrite() === 'in func a');
+});
+
+it('import should pass', function() {
+    var A1 = uproto.import('A'),
+        A2 = uproto.import('test.A');
+    assert(A1 === A2);
+});
+
+it('extends from internal type should pass', function() {
+    var A = uproto.import('A');
+    var B = uproto.extends('B', 'test.A', {
+        init: function() {
+            this.b = 'b';
+        },
+        test_b: function() {
+            return "func b";
+        },
+        overwrite: function() {
+            return "in func b";
+        }
+    });
+
+    var b = new B();
+    assert(b);
+    assert(b instanceof A);
+    assert(b instanceof B);
+    assert(b.a === 'a');
+    assert(b.b === 'b');
+    assert(b.test_a() === 'func a');
+    assert(b.test_b() === 'func b');
+    assert(b.overwrite() === 'in func b');
+});
